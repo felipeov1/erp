c802ace68d9bbb35f2beef87e159bde5cdb21ea5
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2024-02-29 21:27:56 -0300

    feat: parsing body and handle cors

--- api/app.js
+++ api/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan'); //request logs (middleware)
+const bodyParser = require('body-parser');
+
 
 const productsRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.use('/produtos', productsRoutes)
-app.use('/pedidos', orderRoutes)
+app.use((req, res, next) =>{
+    res.header('Acess-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS'){
+        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+});
+
+
+app.use('/produtos', productsRoutes);
+app.use('/pedidos', orderRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
